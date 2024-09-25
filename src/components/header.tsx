564e8d1f6b5a740bbb01517ecac28a0efe410c64
Gabriel_Deon <gabriel1deon@gmail.com>
2024-09-25 10:02:11 -0300

    feat(project): Add shadcn and tailwind to the project

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,9 @@ import styles from '../styles/header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Pixel Ops</h1>
+      <h1>
+        Pixel Ops
+      </h1>      
     </header>
   );
 }

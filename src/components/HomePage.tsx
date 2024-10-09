14270154113f9e796b6c27c9bf208b7577a77c68
Gabriel B. Deon <gabriel1deon@gmail.com>
2024-10-08 21:53:13 -0300

    feat(homepage): Add operations controll layout

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,10 +2,13 @@ import ImageUploader from "./ImageUploader";
 import ImageDownloader from "./ImageDownloader";
 import { useState, useEffect } from "react";
 import { imageMatriceToURL } from "../imageUtils/transformations";
+import ControlerOne from "./controlerOne";
+
 
 export default function HomePage() {  
   const [processedImageUrl, setProcessedImageUrl] = useState<string | null>(null);    
   const [pixelMatrix, setPixelMatrix] = useState<{matrixA: number[][][], matrixB: number[][][]} | null>(null); 
+  //const [resultMatrix, setResultMatrix] = useState<number[][][] | null> (null);
 
   const handleImageProcessed = (matrix: number[][][], mainImage: boolean) => {
     if (mainImage) {
@@ -26,11 +29,11 @@ export default function HomePage() {
 
   return (
     <>
-      <main className="flex flex-col flex-1">
-        <div className="flex flex-row gap-4 mt-5 ml-10 justify-start">
+      <main className="flex flex-col flex-1">      
+        <div className="flex flex-row gap-4 mt-5 ml-24 mr-24 justify-start">
           <ImageUploader onImageProcessed={handleImageProcessed} mainImage={true}/>
           <ImageUploader onImageProcessed={handleImageProcessed} mainImage={false}/>
-
+          <ControlerOne />
           <ImageDownloader
             imageUrl={processedImageUrl}
             altText="Processed Image"
